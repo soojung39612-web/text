7560468afd312502ed823128c9ca4ed6d997a9e6
soojung39612-web <soojung39612@gmail.com>
2026-09-10 10:45:23 +0900

    첫 커밋

---
+++ 학슴.py
@@ -0,0 +1,12 @@
+line = "민수,팔십오"
+
+try:
+    data = line.strip().split(",")
+    name = data[0]
+    score = int(data[1])
+
+except ValueError:
+    print("점수는 숫자로 입력해야 합니다.")
+
+else:
+    print(name, score)
